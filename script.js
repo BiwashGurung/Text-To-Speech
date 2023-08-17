54623911f572c5935b37286255fc3737c15818fb
BiwashGurung <varungrg10@gmail.com>
2023-08-17 13:50:49 +0545

    feat : done with if/else conditon

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ const button = document.querySelector("button");
 let isSpeaking = true;
 
 const textToSpeech = () => {
-    const synth = window.speechSynthesis;
-    const text = textarea.value;
+  const synth = window.speechSynthesis;
+  const text = textarea.value;
 
-    if (!synth.speaking && text) {
-        const utternace = new SpeechSynthesisUtterance(text);
-        synth.speak(utternace);
-      }
+  if (!synth.speaking && text) {
+    const utternace = new SpeechSynthesisUtterance(text);
+    synth.speak(utternace);
+  }
 
-      if (text.length > 50) {
-        if (synth.speaking && isSpeaking) {
-          button.innerText = "Pause";
-          synth.resume();
-          isSpeaking = false;
-        }else {
-            button.innerText = "Resume";
-            synth.pause();
-            isSpeaking = true;
-          }
+  if (text.length > 50) {
+    if (synth.speaking && isSpeaking) {
+      button.innerText = "Pause";
+      synth.resume();
+      isSpeaking = false;
+    } else {
+      button.innerText = "Resume";
+      synth.pause();
+      isSpeaking = true;
+    }
+  } else {
+    isSpeaking = false;
+    button.innerText = "Speaking";
+  }
 };
