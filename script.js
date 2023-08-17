9a55a410f1bccb5c03c233cebab0539e46704983
BiwashGurung <varungrg10@gmail.com>
2023-08-17 13:45:50 +0545

    feat : created function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const textarea = document.querySelector("textarea");
 const button = document.querySelector("button");
 let isSpeaking = true;
+
+const textToSpeech = () => {
+    const synth = window.speechSynthesis;
+    const text = textarea.value;
+};
